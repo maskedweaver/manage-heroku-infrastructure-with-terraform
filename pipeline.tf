0504789d4f728e4139ed221fa93c645f469d5b3c
Nyambati Thomas <thomas.nyambati@andela.com>
2018-09-01 20:29:24 +0300

    attach apps to the pipeline
    Signed-off-by: Nyambati Thomas <thomas.nyambati@andela.com>

--- pipeline.tf
+++ pipeline.tf
@@ -3,3 +3,21 @@
 resource "heroku_pipeline" "pipeline" {
   name = "${var.heroku_pipeline_name}"
 }
+
+resource "heroku_pipeline_coupling" "develop" {
+  app      = "${heroku_app.develop.name}"
+  pipeline = "${heroku_pipeline.pipeline.id}"
+  stage    = "development"
+}
+
+resource "heroku_pipeline_coupling" "staging" {
+  app      = "${heroku_app.staging.name}"
+  pipeline = "${heroku_pipeline.pipeline.id}"
+  stage    = "staging"
+}
+
+resource "heroku_pipeline_coupling" "production" {
+  app      = "${heroku_app.production.name}"
+  pipeline = "${heroku_pipeline.pipeline.id}"
+  stage    = "production"
+}
